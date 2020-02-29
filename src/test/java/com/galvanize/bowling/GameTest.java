028ba8993369a032bcce800d5bd576bc48f4bef5
Sami Akhtar <44879596+Akhtar071988@users.noreply.github.com>
2020-02-29 17:26:28 -0500

    Bowling game kata

--- src/test/java/com/galvanize/bowling/GameTest.java
+++ src/test/java/com/galvanize/bowling/GameTest.java
@@ -1,5 +1,46 @@
 package com.galvanize.bowling;
 
-public class GameTest {
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class GameTest {
+    private Game game;
+
+    @Before
+    public void setUp(){
+        game = new Game();
+    }
+
+    @Test
+    public void canScoreGutterGame(){
+        game.roll(0,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0);
+        assertThat(game.score(), is(0));
+    }
+
+    @Test
+    public void canScoreGameOfOnes(){
+        game.roll(1,1,  1,1,  1,1,  1,1,  1,1,  1,1,  1,1,  1,1,  1,1,  1,1);
+        assertThat(game.score(), is(20));
+    }
+
+    @Test
+    public void canScoreSpareFollowedByThrees() {
+        game.roll(5,5,  3,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0);
+        assertThat(game.score(), is(16));
+    }
+
+    @Test
+    public void canScoreStrikeFollowedByThreeThenThree(){
+        game.roll(10,  3,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0);
+        assertThat(game.score(), is(16));
+    }
+
+    @Test
+    public void canScorePerfectGane(){
+        game.roll(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+        assertThat(game.score(), is(300));
+    }
 }
